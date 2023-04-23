14c2eca26b400e4376e937f099e52cf78fa27dff
Barak Dafna <barakdf@gmail.com>
2023-04-23 09:27:38 +0300

    Init ready project from local

---
+++ Assets/Scripts/AstronautController.cs
@@ -0,0 +1,143 @@
+using System.Collections;
+using UnityEngine;
+
+public class AstronautController : MonoBehaviour
+{
+    public GameObject throwObject;
+    public float throwForce = 10f;
+    public float maxDragDistance = 5f;
+    public float dragMultiplier = 5f;
+    public LineRenderer dragLine;
+    public Camera gameCamera;
+    public float linearDrag = 0.1f;
+    public float angularDrag = 0.1f;
+
+    private Rigidbody2D rb;
+    private Vector3 dragStart;
+    private Vector3 dragEnd;
+    private bool isDragging = false;
+    private float dragDistance = 0f;
+    private Bounds cameraBounds;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.drag = linearDrag;
+        rb.angularDrag = angularDrag;
+        dragLine.enabled = false;
+
+        cameraBounds = new Bounds(gameCamera.transform.position, new Vector3(gameCamera.orthographicSize * gameCamera.aspect * 2, gameCamera.orthographicSize * 2, 0));
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            dragStart = transform.position;
+            isDragging = true;
+            dragDistance = 0f;
+            dragLine.enabled = true;
+            dragLine.SetPosition(0, dragStart);
+            dragLine.SetPosition(1, dragStart);
+        }
+        else if (Input.GetMouseButton(0) && isDragging)
+        {
+            Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mousePosition.z = 0;
+
+            // Clamp the mouse position to the bounds of the camera
+            Vector3 clampedMousePosition = cameraBounds.ClosestPoint(mousePosition);
+            
+            dragDistance = Mathf.Min(Vector3.Distance(clampedMousePosition, dragStart), maxDragDistance);
+            dragEnd = dragStart + (clampedMousePosition - dragStart).normalized * dragDistance;
+            dragLine.SetPosition(1, dragEnd);
+        }
+        else if (Input.GetMouseButtonUp(0) && isDragging)
+        {
+            isDragging = false;
+            GameObject throwObj = Instantiate(throwObject, transform.position, Quaternion.identity);
+            Rigidbody2D throwRb = throwObj.GetComponent<Rigidbody2D>();
+            Vector3 throwDirection = (dragEnd - dragStart).normalized;
+            throwRb.AddForce(throwDirection * dragDistance * dragMultiplier, ForceMode2D.Impulse);
+            rb.AddForce(-throwDirection * dragDistance * dragMultiplier, ForceMode2D.Impulse);
+            dragLine.enabled = false;
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// public class AstronautController : MonoBehaviour
+// {
+//     public GameObject throwObject;
+//     public float throwForce = 10f;
+//     public float maxDragDistance = 5f;
+//     public float dragMultiplier = 5f;
+//     public LineRenderer dragLine;
+
+//     private Rigidbody2D rb;
+//     private Vector3 dragStart;
+//     private Vector3 dragEnd;
+//     private bool isDragging = false;
+//     private float dragDistance = 0f;
+//     private Bounds worldBounds;
+
+//     void Start()
+//     {
+//         rb = GetComponent<Rigidbody2D>();
+//         dragLine.enabled = false;
+
+//         // Get the bounds of the world
+//         worldBounds = new Bounds(Vector3.zero, Vector3.one * 100f);
+//     }
+
+//     void Update()
+//     {
+//         if (Input.GetMouseButtonDown(0))
+//         {
+//             dragStart = transform.position;
+//             isDragging = true;
+//             dragDistance = 0f;
+//             dragLine.enabled = true;
+//             dragLine.SetPosition(0, dragStart);
+//             dragLine.SetPosition(1, dragStart);
+//         }
+//         else if (Input.GetMouseButton(0) && isDragging)
+//         {
+//             Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+//             mousePosition.z = 0;
+
+//             // Clamp the mouse position to the bounds of the world
+//             Vector3 clampedMousePosition = worldBounds.ClosestPoint(mousePosition);
+
+//             dragDistance = Mathf.Min(Vector3.Distance(clampedMousePosition, dragStart), maxDragDistance);
+//             dragEnd = dragStart + (clampedMousePosition - dragStart).normalized * dragDistance;
+//             dragLine.SetPosition(1, dragEnd);
+//         }
+//         else if (Input.GetMouseButtonUp(0) && isDragging)
+//         {
+//             isDragging = false;
+//             GameObject throwObj = Instantiate(throwObject, transform.position, Quaternion.identity);
+//             Rigidbody2D throwRb = throwObj.GetComponent<Rigidbody2D>();
+//             Vector3 throwDirection = (dragEnd - dragStart).normalized;
+//             throwRb.AddForce(throwDirection * dragDistance * dragMultiplier, ForceMode2D.Impulse);
+//             rb.AddForce(-throwDirection * dragDistance * dragMultiplier, ForceMode2D.Impulse);
+//             dragLine.enabled = false;
+//         }
+//     }
+// }
---
+++ Assets/Scripts/PipeThrower.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class PipeThrower : MonoBehaviour
+{
+    public float throwForce = 3f;
+    [SerializeField] Transform ExitPipe;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Check if the collision was with the astronaut
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            float impactStrength = collision.relativeVelocity.magnitude;
+            // Calculate the throw direction and apply force to the astronaut
+            Debug.Log("Got Collision");
+            collision.gameObject.transform.position = ExitPipe.position;
+            Vector2 throwDirection = ExitPipe.position - transform.position; // use the pipe's right direction
+            Debug.Log("Direction Vector " + throwDirection);
+            collision.gameObject.GetComponent<Rigidbody2D>().AddForce(throwDirection * impactStrength * throwForce, ForceMode2D.Impulse);
+        }
+    }   
+}
