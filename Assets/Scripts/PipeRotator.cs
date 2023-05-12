ec61af49d99a12fa61e6d395cfd1e0a406e64d13
Barak Dafna <barakdf@gmail.com>
2023-05-12 17:02:31 +0300

    Pipe Rotator

---
+++ Assets/Scripts/PipeRotator.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class PipeRotator : MonoBehaviour {
+    [FormerlySerializedAs("directionUp")] [SerializeReference] private bool directionRight;
+
+    [SerializeField] private float speed;
+
+    private Transform _transform;
+    // Start is called before the first frame update
+    void Start() {
+        _transform = gameObject.GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (directionRight) {
+            //change the Z-axis 
+            _transform.Rotate(0, 0, speed * Time.deltaTime);
+        }
+        else {
+            _transform.Rotate(0, 0, -speed * Time.deltaTime);
+        }
+    }
+}
