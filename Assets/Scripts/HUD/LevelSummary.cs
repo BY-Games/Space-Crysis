9d0ae2064e656699ac16c194e66e7edd0aeb4a97
Barak Dafna <barakdf@gmail.com>
2023-05-22 23:11:21 +0300

    changed background and removed pipes debugging sprites

--- Assets/Scripts/HUD/LevelSummary.cs
+++ Assets/Scripts/HUD/LevelSummary.cs
@@ -16,6 +16,7 @@ public class LevelSummary : MonoBehaviour {
         }
         GameManager.OnGameStateChange += ShowGameSummaryOnState;
     }
+    
 
     private void OnDestroy() {
         GameManager.OnGameStateChange -= ShowGameSummaryOnState;
