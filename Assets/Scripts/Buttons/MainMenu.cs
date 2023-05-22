b05816da872cb15ec41af95c640846ab73c6f1a7
yosefkahlon <yosefkahlon53@gmail.com>
2023-05-22 12:43:38 +0300

    menu

---
+++ Assets/Scripts/Buttons/MainMenu.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainMenu : MonoBehaviour
+{
+    
+    public void Play()
+    {
+        //set the next scence in the order of the build. 
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
+    }
+}
