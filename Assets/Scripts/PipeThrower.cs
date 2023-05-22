294cde6c52ab41aa814b5162d38bdb83aaefe7fc
Barak Dafna <barakdf@gmail.com>
2023-05-22 14:04:24 +0300

    Update PipeThrower.cs

--- Assets/Scripts/PipeThrower.cs
+++ Assets/Scripts/PipeThrower.cs
@@ -17,18 +17,24 @@ public class PipeThrower : MonoBehaviour
             // Calculate the throw direction and apply force to the astronaut
             var position = ExitPipe.position;
             Vector2 throwDirection;
+            Debug.Log("Position vec" + position);
             if (straight) {
                 throwDirection = position - transform.position; // use the pipe's right direction
             }
             else {
                 throwDirection = position; // use the pipe's right direction
                 throwDirection.x *= -1;
+                Debug.Log("Throw vec " + throwDirection);
             }
+            Debug.Log("Before change pos" + collision.gameObject.transform.position);
             collision.gameObject.transform.position = position; // Teleport to other side of pipe
+            // collision.gameObject.transform.position.x *= -1;
+            Debug.Log("Before AFTER pos" + collision.gameObject.transform.position);
+
             // Debug.Log("Direction Vector " + throwDirection);
             
             // Force applied on player when exit the pipe
-            Vector3 exitPipeForce = throwDirection * impactStrength * throwForce;
+            Vector3 exitPipeForce = throwDirection;
             collision.gameObject.GetComponent<Rigidbody2D>().AddForce(exitPipeForce, ForceMode2D.Impulse);
         }
     }   
