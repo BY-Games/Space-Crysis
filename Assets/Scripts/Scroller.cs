ed1e0c1deac97f5b729fb167a6161aec2f9b0fb5
yosefkahlon <yosefkahlon53@gmail.com>
2023-06-10 14:01:27 +0300

    scrolling background

---
+++ Assets/Scripts/Scroller.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Scroller : MonoBehaviour
+{
+    [SerializeField] RawImage img;
+   float x = 0.01f , y = 0.01f ;
+
+    // Update is called once per frame
+    void Update()
+    {
+        img.uvRect = new Rect(img.uvRect.position + new Vector2(x, y) * Time.deltaTime, img.uvRect.size);
+          
+    }
+}
