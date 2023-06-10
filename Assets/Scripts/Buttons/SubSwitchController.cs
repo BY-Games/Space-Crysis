d61020a593c3589071d1f98672fe69643d71c4bf
Barak Dafna <barakdf@gmail.com>
2023-06-10 16:27:06 +0300

    changed winningwall physic material and size of tiles

--- Assets/Scripts/Buttons/SubSwitchController.cs
+++ Assets/Scripts/Buttons/SubSwitchController.cs
@@ -5,25 +5,27 @@ using UnityEngine;
 using UnityEngine.Serialization;
 
 public class SubSwitchController : MonoBehaviour {
-    [FormerlySerializedAs("Blades")] [SerializeField] private Floater[] blades;
+    [FormerlySerializedAs("Blades")] [SerializeField]
+    private Floater[] blades;
+
     [SerializeField] private GameObject switchOn;
-    [FormerlySerializedAs("switchOf")] [SerializeField] private GameObject switchOff;
+
+    [FormerlySerializedAs("switchOf")] [SerializeField]
+    private GameObject switchOff;
 
     // Start is called before the first frame update
     void Start() {
         switchOn.SetActive(false);
         switchOff.SetActive(true);
     }
-    
+
 
     private void OnTriggerEnter2D(Collider2D other) {
-        if (other.gameObject.CompareTag("Tool")) {
-            Debug.Log("Here");
-            switchOff.SetActive(false);
-            switchOn.SetActive(true);
-            foreach (Floater bladeFloater in blades) {
-                bladeFloater.StopMove();
-            }
+        Debug.Log("Here");
+        switchOff.SetActive(false);
+        switchOn.SetActive(true);
+        foreach (Floater bladeFloater in blades) {
+            bladeFloater.StopMove();
         }
     }
 }
