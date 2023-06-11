1261de055870fe63e5d7b7c5a8377fab848d1f63
Barak Dafna <barakdf@gmail.com>
2023-06-11 22:19:52 +0300

    first scene is opening cutscene

--- Assets/Scripts/HUD/MoveToNextLevel.cs
+++ Assets/Scripts/HUD/MoveToNextLevel.cs
@@ -1,29 +1,12 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.InputSystem;
-using UnityEngine.SceneManagement;
-
-
-public class MoveToNextLevel : MonoBehaviour
-{
-//         [SerializeField] InputAction nextLevel = new InputAction(type: InputActionType.Button);
-
-
-//  void OnEnable()  {
-//         nextLevel.Enable();
+// using UnityEngine;
+//
+//
+//
+// public class MoveToNextLevel : MonoBehaviour
+// {
+//     private void Start() {
+//         StartCoroutine(LevelManager.Instance.LoadScenes());
 //     }
-
-//     void OnDisable()  {
-//         nextLevel.Disable();
-//     }
-
-//     // Update is called once per frame
-//     void Update()
-//     {
-//         if(nextLevel.WasPressedThisFrame()) {
-//             SceneManager.LoadScene("Level 1");
-//             gameObject.SetActive(false);
-//         }
-//     }
-}
+//
+//     
+// }
