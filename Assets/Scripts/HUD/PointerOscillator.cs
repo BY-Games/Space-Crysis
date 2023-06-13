e012fd4fcffcd899cf8e9228c61b82f5577536e9
Barak Dafna <barakdf@gmail.com>
2023-06-13 15:47:44 +0300

    level 1-b update UI

--- Assets/Scripts/HUD/PointerOscillator.cs
+++ Assets/Scripts/HUD/PointerOscillator.cs
@@ -2,22 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PointerOscillator : MonoBehaviour
-{
-    float range = 0.1f;
-     uint speed = 2;
+public class PointerOscillator : MonoBehaviour {
+    [SerializeReference] private bool horizontal = false;
+    [SerializeField] private float range = 0.1f;
+    [SerializeField] private uint speed = 2;
     float axis = 0;
-    float point;    
+    float point;
 
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         // Debug.Log(transform.position);
-        float sinCycle = (2*Mathf.PI);
+        float sinCycle = (2 * Mathf.PI);
         axis = (axis % sinCycle) + Time.deltaTime * speed;
         point = Mathf.Sin(axis) * range;
-        GetComponent<Transform>().position = new Vector3(transform.position.x,transform.position.y + point,transform.position.z); 
-       
+        if (horizontal) {
+            GetComponent<Transform>().position =
+                new Vector3(transform.position.x  + point, transform.position.y, transform.position.z);
+        }
+        else {
+            GetComponent<Transform>().position =
+                new Vector3(transform.position.x, transform.position.y + point, transform.position.z);
+        }
     }
 }
